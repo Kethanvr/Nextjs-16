0240d43d33e0bd3788602941ed3f026af49ebb3b
kethan VR <kethanvr@gmail.com>
2025-11-01 22:02:05 +0530

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  reactCompiler: true,
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
